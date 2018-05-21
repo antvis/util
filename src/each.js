c7fddb8477fe9df7d69f14af77e84fd157bd47b4
Elaine1234 <yilin.qyl@antfin.com>
2018-05-21 19:48:14 +0800

    fix(each.js): return false in array won't break the loop

--- src/each.js
+++ src/each.js
@@ -1,23 +1,25 @@
 const isObject = require('./type/isObject');
+const isArray = require('./type/isArray');
+
 const each = function(elements, func) {
   if (!elements) {
     return;
   }
   let rst;
-  if (isObject(elements)) {
+  if (isArray(elements)) {
+    for (let i = 0; i < elements.length; i++) {
+      rst = func(elements[i], i);
+      if (rst === false) {
+        break;
+      }
+    }
+  } else if (isObject(elements)) {
     for (const k in elements) {
       if (elements.hasOwnProperty(k)) {
         rst = func(elements[k], k);
         if (rst === false) {
           break;
         }
-      }
-    }
-  } else if (elements.length) {
-    for (let i = 0; i < elements.length; i++) {
-      rst = func(elements[i], i);
-      if (rst === false) {
-        break;
       }
     }
   }
