56a6da084ae2379645268eeb837d655f1cc32a31
Elaine1234 <yilin.qyl@antfin.com>
2018-05-22 10:27:26 +0800

    test(map-spec): add unit test of function map

--- src/map.js
+++ src/map.js
@@ -1,6 +1,10 @@
 const each = require('./each');
+const isArrayLike = require('./type/isArrayLike');
 
 const map = function(arr, func) {
+  if (!isArrayLike(arr)) {
+    return arr;
+  }
   const result = [];
   each(arr, function(value, index) {
     result.push(func(value, index));
---
+++ test/unit/map-spec.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+const map = require('../../src/map');
+
+describe('map', () => {
+  it('map array', () => {
+    const a = [ 1, 2, 3 ];
+    const b = map(a, item => { return ++item; });
+    expect(a.length).to.equal(3);
+    expect(b.length).to.equal(3);
+    expect(a[0]).to.equal(1);
+    expect(b[0]).to.equal(2);
+
+    const c = map(null, item => { return ++item; });
+    expect(c).to.equal(null);
+  });
+});
