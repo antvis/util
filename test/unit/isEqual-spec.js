899e9d7bb82f9409994701f3ac7a22b3904a2197
Elaine1234 <yilin.qyl@antfin.com>
2018-06-13 17:04:52 +0800

    feat: add unit tests of isEqualWith

--- test/unit/isEqual-spec.js
+++ test/unit/isEqual-spec.js
@@ -1,8 +1,9 @@
 const expect = require('chai').expect;
 const isEqual = require('../../src/isEqual');
+const isEqualWith = require('../../src/isEqualWith');
 
-describe('compare arrays', () => {
-  it('原始类型数组', () => {
+describe('isEqual', () => {
+  it('primitive arrays', () => {
     expect(isEqual()).to.be.true;
     expect(isEqual([ 1, 2 ], [ 1, 2 ])).to.be.true;
     expect(isEqual([ 1, 2 ], [ 1, 3 ])).to.be.false;
@@ -12,22 +13,37 @@ describe('compare arrays', () => {
     expect(isEqual([ 1, 2 ], [ 1, 2, 3 ])).to.be.false;
     expect(isEqual([ 'ab', 'cd' ], [ 'ed', 'as' ])).to.be.false;
   });
-  it('对象数组', () => {
+  it('object arrays', () => {
     expect(isEqual([{ a: 1 }], [{ a: 1 }])).to.be.true;
     expect(isEqual([{ a: 111 }, { a: 222 }], [{ a: 111 }, { a: 222 }])).to.be.true;
     expect(isEqual([{ a: 11, b: 22 }, { a: 11, c: 22 }])).to.be.false;
   });
-  it('空数组', () => {
+  it('empty arrays', () => {
     expect(isEqual([], [])).to.be.true;
     expect(isEqual([ null ], [ null ])).to.be.true;
     expect(isEqual([ undefined ], [ NaN ])).to.be.false;
   });
+  it('compare objects', () => {
+    expect(isEqual({}, {})).to.be.true;
+    expect(isEqual({ a: 111 }, { a: 111 })).to.be.true;
+    expect(isEqual({ a: 1, b: 2 }, { a: 1 })).to.be.false;
+    expect(isEqual({ a: [ 1, 2 ] }, { a: [ 1, 2 ] })).to.be.true;
+    expect(isEqual({ a: [ '1', '2' ] }, { a: [ '1' ] })).to.be.false;
+  });
 });
 
-describe('compare objects', () => {
-  expect(isEqual({}, {})).to.be.true;
-  expect(isEqual({ a: 111 }, { a: 111 })).to.be.true;
-  expect(isEqual({ a: 1, b: 2 }, { a: 1 })).to.be.false;
-  expect(isEqual({ a: [ 1, 2 ] }, { a: [ 1, 2 ] })).to.be.true;
-  expect(isEqual({ a: [ '1', '2' ] }, { a: [ '1' ] })).to.be.false;
+describe('isEqualWidth', () => {
+  it('isEqualWith with and without fn', () => {
+    function isGreeting(value) {
+      return /^h(?:i|ello)$/.test(value);
+    }
+    function fn(objValue, othValue) {
+      if (isGreeting(objValue) && isGreeting(othValue)) {
+        return true;
+      }
+    }
+    expect(isEqualWith('hello', 'hi', fn)).to.be.true;
+    expect(isEqualWith('hello', 'hi')).to.be.false;
+  });
 });
+
