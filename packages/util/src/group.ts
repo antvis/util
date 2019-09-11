781f34e99604e56019e1d4aa70a7e9590fd41a89
simaQ <sima.zhang1990@gmail.com>
2019-09-11 11:42:05 +0800

    `condition` can be string[]

--- packages/util/src/group.ts
+++ packages/util/src/group.ts
@@ -1,6 +1,6 @@
 import groupToMap from './group-to-map';
 
-export default <T>(data: T[], condition: ((v: T) => string) | string): T[][] => {
+export default <T>(data: T[], condition: ((v: T) => string) | string | string[]): T[][] => {
   if (!condition) {
     // 没有条件，则自身改成数组
     return [ data ];
