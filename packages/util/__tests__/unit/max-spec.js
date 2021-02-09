56c570d8934f30ae1ea347ddbea843ef1b3216be
xinming <736929286@qq.com>
2021-02-09 20:06:11 +0800

    test: 添加 max、min单测

---
+++ packages/util/__tests__/unit/max-spec.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+import { max } from '../../src';
+
+describe('max', () => {
+  it('primitive arrays', () => {
+    expect(max([1, 2])).to.be.equal(2);
+  });
+
+  it('empty arrays', () => {
+    expect(max([])).to.be.equal(undefined);
+  });
+
+  it('large arrays', () => {
+    const data = new Array(1250010).fill(1).map((d, idx) => idx);
+    expect(max(data)).to.be.equal(1250009);
+
+    expect(() => Math.max(...data)).to.be.throw('Maximum call stack size exceeded');
+  });
+});
---
+++ packages/util/__tests__/unit/min-spec.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+import { min } from '../../src';
+
+describe('min', () => {
+  it('primitive arrays', () => {
+    expect(min([1, 2])).to.be.equal(1);
+  });
+
+  it('empty arrays', () => {
+    expect(min([])).to.be.equal(undefined);
+  });
+
+  it('large arrays', () => {
+    const data = new Array(1250010).fill(1).map((d, idx) => idx);
+    expect(min(data)).to.be.equal(0);
+    expect(() => Math.min(...data)).to.be.throw('Maximum call stack size exceeded');
+  });
+});
