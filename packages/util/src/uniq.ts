80318399a0ec4c81a55596e0f47cfd59521bbe72
hustcc <i@hust.cc>
2021-02-20 12:25:10 +0800

    perf: use for loop replace for of

--- packages/util/src/uniq.ts
+++ packages/util/src/uniq.ts
@@ -2,7 +2,8 @@ export default function uniq(arr: any[], cache = new Map()) {
   const r = [];
 
   if (Array.isArray(arr)) {
-    for (const item of arr) {
+    for (let i = 0, len = arr.length; i < len; i ++) {
+      const item = arr[i];
       // 加一个 cache，提升性能
       if (!cache.has(item)) {
         r.push(item);
