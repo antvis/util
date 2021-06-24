c3bc53baff08ec6f27a9ed0daaeb4c0b70328186
hustcc <i@hust.cc>
2021-06-24 16:58:25 +0800

    chore: fix unit test

--- packages/util/__tests__/unit/max-by-spec.js
+++ packages/util/__tests__/unit/max-by-spec.js
@@ -3,11 +3,11 @@ import { maxBy } from '../../src';
 
 describe('maxBy', () => {
   it('primitive object arrays', () => {
-    expect(maxBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal(2);
-    expect(maxBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(-Infinity);
+    expect(maxBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal({ n: 2 });
+    expect(maxBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(undefined);
 
-    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal(2);
-    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(-Infinity);
+    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal({ n: 2 });
+    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(undefined);
   });
 
   it('empty arrays', () => {
--- packages/util/__tests__/unit/min-by-spec.js
+++ packages/util/__tests__/unit/min-by-spec.js
@@ -3,11 +3,11 @@ import { minBy } from '../../src';
 
 describe('minBy', () => {
   it('primitive object arrays', () => {
-    expect(minBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal(2);
-    expect(minBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(Infinity);
+    expect(minBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal({ n: 1 });
+    expect(minBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(undefined);
 
-    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal(2);
-    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(Infinity);
+    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal({ n: 1 });
+    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(undefined);
   });
 
   it('empty arrays', () => {
