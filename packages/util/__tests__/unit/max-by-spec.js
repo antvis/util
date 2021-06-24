91cc26d6ada56c68c71c06a71d0dd0d3239014c0
hustcc <i@hust.cc>
2021-06-24 17:11:44 +0800

    chore: fix unit test

--- packages/util/__tests__/unit/max-by-spec.js
+++ packages/util/__tests__/unit/max-by-spec.js
@@ -3,15 +3,15 @@ import { maxBy } from '../../src';
 
 describe('maxBy', () => {
   it('primitive object arrays', () => {
-    expect(maxBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal({ n: 2 });
-    expect(maxBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(undefined);
+    expect(maxBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.eql({ n: 2 });
+    expect(maxBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.eql(undefined);
 
-    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal({ n: 2 });
-    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(undefined);
+    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.eql({ n: 2 });
+    expect(maxBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.eql(undefined);
   });
 
   it('empty arrays', () => {
-    expect(maxBy([], 'v')).to.be.equal(undefined);
-    expect(maxBy([], (d) => d.v)).to.be.equal(undefined);
+    expect(maxBy([], 'v')).to.be.eql(undefined);
+    expect(maxBy([], (d) => d.v)).to.be.eql(undefined);
   });
 });
--- packages/util/__tests__/unit/min-by-spec.js
+++ packages/util/__tests__/unit/min-by-spec.js
@@ -3,15 +3,15 @@ import { minBy } from '../../src';
 
 describe('minBy', () => {
   it('primitive object arrays', () => {
-    expect(minBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.equal({ n: 1 });
-    expect(minBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.equal(undefined);
+    expect(minBy([ { n: 1 }, { n: 2 } ], 'n')).to.be.eql({ n: 1 });
+    expect(minBy([ { n: 1 }, { n: 2 } ], 'v')).to.be.eql(undefined);
 
-    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.equal({ n: 1 });
-    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.equal(undefined);
+    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.n)).to.be.eql({ n: 1 });
+    expect(minBy([ { n: 1 }, { n: 2 } ], (d) => d.v)).to.be.eql(undefined);
   });
 
   it('empty arrays', () => {
-    expect(minBy([], 'v')).to.be.equal(undefined);
-    expect(minBy([], (d) => d.v)).to.be.equal(undefined);
+    expect(minBy([], 'v')).to.be.eql(undefined);
+    expect(minBy([], (d) => d.v)).to.be.eql(undefined);
   });
 });
