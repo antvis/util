2a7ae0308dbc15c8dd4978d2249c275cdaf8c6f5
yangtao.yangtao <yangtao.yangtao@alibaba-inc.com>
2021-09-28 16:02:35 +0800

    refactor(path-util): 更新了 catmullRom2Bezier 方法实现

--- packages/path-util/src/catmull-rom-2-bezier.ts
+++ packages/path-util/src/catmull-rom-2-bezier.ts
@@ -1,61 +1,143 @@
-// http://schepers.cc/getting-to-the-point
-export default function catmullRom2Bezier(crp: number[], z?: boolean): any[][] {
-  const d = [];
-  // @ts-ignore
-  for (let i = 0, iLen = crp.length; iLen - 2 * !z > i; i += 2) {
-    const p = [ {
-      x: +crp[i - 2],
-      y: +crp[i - 1],
-    }, {
-      x: +crp[i],
-      y: +crp[i + 1],
-    }, {
-      x: +crp[i + 2],
-      y: +crp[i + 3],
-    }, {
-      x: +crp[i + 4],
-      y: +crp[i + 5],
-    } ];
-    if (z) {
-      if (!i) {
-        p[0] = {
-          x: +crp[iLen - 2],
-          y: +crp[iLen - 1],
-        };
-      } else if (iLen - 4 === i) {
-        p[3] = {
-          x: +crp[0],
-          y: +crp[1],
-        };
-      } else if (iLen - 2 === i) {
-        p[2] = {
-          x: +crp[0],
-          y: +crp[1],
-        };
-        p[3] = {
-          x: +crp[2],
-          y: +crp[3],
-        };
-      }
-    } else {
-      if (iLen - 4 === i) {
-        p[3] = p[2];
-      } else if (!i) {
-        p[0] = {
-          x: +crp[i],
-          y: +crp[i + 1],
-        };
-      }
+import { vec2 } from '@antv/matrix-util';
+import type { PathCommand } from './types';
+
+type Pos = [number, number];
+
+function smoothBezier(points: Pos[], smooth: number, isLoop: boolean, constraint: Pos[]) {
+  const cps: vec2[] = [];
+  const hasConstraint = !!constraint;
+
+  let prevPoint: Pos;
+  let nextPoint: Pos;
+  let min: vec2;
+  let max: vec2;
+  let nextCp0: vec2;
+  let cp1: vec2;
+  let cp0: vec2;
+
+  if (hasConstraint) {
+    [ min, max ] = constraint;
+    for (let i = 0, l = points.length; i < l; i += 1) {
+      const point = points[i];
+      min = vec2.min([ 0, 0 ], min, point);
+      max = vec2.max([ 0, 0 ], max, point);
     }
-    d.push([ 'C',
-      (-p[0].x + 6 * p[1].x + p[2].x) / 6,
-      (-p[0].y + 6 * p[1].y + p[2].y) / 6,
-      (p[1].x + 6 * p[2].x - p[3].x) / 6,
-      (p[1].y + 6 * p[2].y - p[3].y) / 6,
-      p[2].x,
-      p[2].y,
-    ]);
   }
 
-  return d;
+  for (let i = 0, len = points.length; i < len; i += 1) {
+    const point = points[i];
+    if (i === 0 && !isLoop) {
+      cp0 = point;
+    } else if (i === len - 1 && !isLoop) {
+      cp1 = point;
+      cps.push(cp0);
+      cps.push(cp1);
+    } else {
+      const prevIdx = [ i ? i - 1 : len - 1, i - 1 ][isLoop ? 0 : 1];
+      prevPoint = points[prevIdx];
+      nextPoint = points[isLoop ? (i + 1) % len : i + 1];
+
+      let v: vec2 = [ 0, 0 ];
+      v = vec2.sub(v, nextPoint, prevPoint);
+      v = vec2.scale(v, v, smooth);
+
+      let d0 = vec2.distance(point, prevPoint);
+      let d1 = vec2.distance(point, nextPoint);
+
+      const sum = d0 + d1;
+      if (sum !== 0) {
+        d0 /= sum;
+        d1 /= sum;
+      }
+
+      let v1 = vec2.scale([ 0, 0 ], v, -d0);
+      let v2 = vec2.scale([ 0, 0 ], v, d1);
+
+      cp1 = vec2.add([ 0, 0 ], point, v1);
+      nextCp0 = vec2.add([ 0, 0 ], point, v2);
+
+      // 下一个控制点必须在这个点和下一个点之间
+      nextCp0 = vec2.min([ 0, 0 ], nextCp0, vec2.max([ 0, 0 ], nextPoint, point));
+      nextCp0 = vec2.max([ 0, 0 ], nextCp0, vec2.min([ 0, 0 ], nextPoint, point));
+
+      // 重新计算 cp1 的值
+      v1 = vec2.sub([ 0, 0 ], nextCp0, point);
+      v1 = vec2.scale([ 0, 0 ], v1, -d0 / d1);
+      cp1 = vec2.add([ 0, 0 ], point, v1);
+
+      // 上一个控制点必须要在上一个点和这一个点之间
+      cp1 = vec2.min([ 0, 0 ], cp1, vec2.max([ 0, 0 ], prevPoint, point));
+      cp1 = vec2.max([ 0, 0 ], cp1, vec2.min([ 0, 0 ], prevPoint, point));
+
+      // 重新计算 nextCp0 的值
+      v2 = vec2.sub([ 0, 0 ], point, cp1);
+      v2 = vec2.scale([ 0, 0 ], v2, d1 / d0);
+      nextCp0 = vec2.add([ 0, 0 ], point, v2);
+
+      if (hasConstraint) {
+        cp1 = vec2.max([ 0, 0 ], cp1, min);
+        cp1 = vec2.min([ 0, 0 ], cp1, max);
+        nextCp0 = vec2.max([ 0, 0 ], nextCp0, min);
+        nextCp0 = vec2.min([ 0, 0 ], nextCp0, max);
+      }
+
+      cps.push(cp0);
+      cps.push(cp1);
+      cp0 = nextCp0;
+    }
+  }
+
+  if (isLoop) {
+    cps.push(cps.shift());
+  }
+
+  return cps;
 }
+
+/**
+ * create bezier spline from catmull rom spline
+ * @param {Array} crp Catmull Rom Points
+ * @param {boolean} z Spline is loop
+ * @param {Array} constraint Constraint
+ */
+function catmullRom2Bezier(
+  crp: number[],
+  z: boolean = false,
+  constraint: Pos[] = [
+    [ 0, 0 ],
+    [ 1, 1 ],
+  ]
+): PathCommand[] {
+  const isLoop = !!z;
+  const pointList: Pos[] = [];
+  for (let i = 0, l = crp.length; i < l; i += 2) {
+    pointList.push([ crp[i], crp[i + 1] ]);
+  }
+
+  const controlPointList = smoothBezier(pointList, 0.4, isLoop, constraint);
+  const len = pointList.length;
+  const d1: PathCommand[] = [];
+
+  let cp1: vec2;
+  let cp2: vec2;
+  let p: Pos;
+
+  for (let i = 0; i < len - 1; i += 1) {
+    cp1 = controlPointList[i * 2];
+    cp2 = controlPointList[i * 2 + 1];
+    p = pointList[i + 1];
+
+    d1.push([ 'C', cp1[0], cp1[1], cp2[0], cp2[1], p[0], p[1] ]);
+  }
+
+  if (isLoop) {
+    cp1 = controlPointList[len];
+    cp2 = controlPointList[len + 1];
+    [ p ] = pointList;
+
+    d1.push([ 'C', cp1[0], cp1[1], cp2[0], cp2[1], p[0], p[1] ]);
+  }
+  return d1;
+}
+
+export default catmullRom2Bezier;
