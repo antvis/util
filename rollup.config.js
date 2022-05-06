438b3bda7a06f00ef81e4611bb73465a6bba5418
hustcc <i@hust.cc>
2022-05-06 13:44:09 +0800

    chore: upgrade rollup plugin typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { uglify } from 'rollup-plugin-uglify';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
 
