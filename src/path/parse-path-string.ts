de5d976329ca0199131b5af9ae67109714e79120
yuqi.pyq <yuqi.pyq@antgroup.com>
2022-05-22 18:20:11 +0800

    fix: parse path to string correctly

--- src/path/parse-path-string.ts
+++ src/path/parse-path-string.ts
@@ -6,57 +6,57 @@ const PATH_COMMAND = new RegExp(
 const PATH_VALUES = new RegExp('(-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?)[' + SPACES + ']*,?[' + SPACES + ']*', 'ig');
 
 // Parses given path string into an array of arrays of path segments
-export function parsePathString(pathString: string) {
-  if (!pathString) {
-    return null;
-  }
+export function parsePathString(pathString: string | any[]) {
+  if (typeof pathString === 'string') {
+    if (pathString) {
+      const paramCounts = {
+        a: 7,
+        c: 6,
+        o: 2,
+        h: 1,
+        l: 2,
+        m: 2,
+        r: 4,
+        q: 4,
+        s: 4,
+        t: 2,
+        v: 1,
+        u: 3,
+        z: 0,
+      };
+      const data = [];
 
-  if (Array.isArray(pathString)) {
+      pathString.replace(PATH_COMMAND, function (a, b, c) {
+        const params = [];
+        let name = b.toLowerCase();
+        c.replace(PATH_VALUES, function (a, b) {
+          b && params.push(+b);
+        });
+        if (name === 'm' && params.length > 2) {
+          data.push([b].concat(params.splice(0, 2)));
+          name = 'l';
+          b = b === 'm' ? 'l' : 'L';
+        }
+        if (name === 'o' && params.length === 1) {
+          data.push([b, params[0]]);
+        }
+        if (name === 'r') {
+          data.push([b].concat(params));
+        } else {
+          while (params.length >= paramCounts[name]) {
+            data.push([b].concat(params.splice(0, paramCounts[name])));
+            if (!paramCounts[name]) {
+              break;
+            }
+          }
+        }
+        return '';
+      });
+      return data;
+    } else {
+      return null;
+    }
+  } else if (Array.isArray(pathString)) {
     return pathString;
   }
-  const paramCounts = {
-    a: 7,
-    c: 6,
-    o: 2,
-    h: 1,
-    l: 2,
-    m: 2,
-    r: 4,
-    q: 4,
-    s: 4,
-    t: 2,
-    v: 1,
-    u: 3,
-    z: 0,
-  };
-  const data = [];
-
-  String(pathString).replace(PATH_COMMAND, function (a, b, c) {
-    const params = [];
-    let name = b.toLowerCase();
-    c.replace(PATH_VALUES, function (a, b) {
-      b && params.push(+b);
-    });
-    if (name === 'm' && params.length > 2) {
-      data.push([b].concat(params.splice(0, 2)));
-      name = 'l';
-      b = b === 'm' ? 'l' : 'L';
-    }
-    if (name === 'o' && params.length === 1) {
-      data.push([b, params[0]]);
-    }
-    if (name === 'r') {
-      data.push([b].concat(params));
-    } else {
-      while (params.length >= paramCounts[name]) {
-        data.push([b].concat(params.splice(0, paramCounts[name])));
-        if (!paramCounts[name]) {
-          break;
-        }
-      }
-    }
-    return '';
-  });
-
-  return data;
 }
